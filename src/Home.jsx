33ddf76544c74665ee0dbd20d1635ae4606bece1
rakeshyelledi <rakeshyelledi@gmail.com>
2025-07-31 21:19:43 +0530

    worked on useEffect Hook

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react'
+import React, { use, useEffect, useState } from 'react'
 
 const Home = () => {
   const [value, setvalue] = useState('');
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState(2);
+  const [show,setShow]=useState(true);
 
   const handelInput = (event) => {
     setInputValue(event.target.value)
@@ -10,12 +11,25 @@ const Home = () => {
   const handelvalue = () => {
     setvalue(inputValue);
   }
+  const handelchange=()=>{
+    setShow(!show)
+  }
+  const handelDouble = () => {
+    setInputValue(inputValue*2)
+  }
+  useEffect(()=>{
+    document.title=`clicked ${inputValue} times`
+  },[inputValue])
   return (
     <div>
       <h1>You Types is : {inputValue}</h1>
       <h1>Value is : {value}</h1>
+      <button onClick={handelDouble}>Double</button>
       <input type="text" onChange={handelInput} />
       <button onClick={handelvalue}>submit</button>
+      <button onClick={handelchange} style={{backgroundColor: show? 'red':'green', color:'white'}}>{show?'Hide':'Show'}</button>
+      {show && <h1>Welcome to React</h1>}
+      
     </div>
   )
 }
