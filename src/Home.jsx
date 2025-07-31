ca9ec49b9ed11cda0c57e7f1936be60a6ead4fd7
rakeshyelledi <rakeshyelledi@gmail.com>
2025-07-31 12:23:17 +0530

    Worked on some useState examples

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Home = () => {
+  const [value, setvalue] = useState('');
+  const [inputValue, setInputValue] = useState('');
+
+  const handelInput = (event) => {
+    setInputValue(event.target.value)
+  }
+  const handelvalue = () => {
+    setvalue(inputValue);
+  }
   return (
     <div>
-      Home  
+      <h1>You Types is : {inputValue}</h1>
+      <h1>Value is : {value}</h1>
+      <input type="text" onChange={handelInput} />
+      <button onClick={handelvalue}>submit</button>
     </div>
   )
 }
 
 export default Home
- 
